9e77152d6296981f14f7f73155ad978f36bf2ec5
Niek <niek_vs@hotmail.com>
2018-08-01 11:53:25 +0200

    Add mustache templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,23 +10,33 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        createdAt: formattedTime,
+        from: message.from
+    });
 
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
+    // var li = jQuery('<li></li>');
+    // var a = jQuery('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
+    // li.text(`${message.from} ${formattedTime}: `);
+    // a.attr('href', message.url);
+    // li.append(a);
 
-    jQuery('#messages').append(li);
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        url: message.url,
+        createdAt: formattedTime,
+        from: message.from
+    });
+
+    jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
@@ -52,7 +62,7 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by the browser');
     };
 
-    locationButton.attr('disabled','disabled').text('Sending location....');
+    locationButton.attr('disabled', 'disabled').text('Sending location....');
 
     navigator.geolocation.getCurrentPosition(function (position) {
         locationButton.removeAttr('disabled').text('Send location');
